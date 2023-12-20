f0ceb5c0bb2a9ae8794214d01b80e4ed64810515
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2023-12-21 01:41:49 +0530

    update props in tab button and examples

--- 01-Reactjs-Basic/src/components/Section.jsx
+++ 01-Reactjs-Basic/src/components/Section.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Section({title, id, children}) {
+function Section({title,children, ...props}) {
   return (
-    <section id={id}>
+    <section {...props}>
         <h2>{title}</h2>
         {children}
     </section>
