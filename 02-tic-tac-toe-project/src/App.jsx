137a1a45086a45dca121b07e878cda03e29c7c52
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2023-12-22 00:57:56 +0530

    added player 1 and player 2

--- 02-tic-tac-toe-project/src/App.jsx
+++ 02-tic-tac-toe-project/src/App.jsx
@@ -3,7 +3,29 @@ function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+          <span>
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+
+          </span>
+          <button>Edit</button>
+         </li>
+
+          <li>
+          <span>
+          <span className="player-name">Player 2</span>
+          <span className="player-symbol">O</span>
+          </span>
+          <button>Edit</button>
+
+          </li>
+        </ol>
+      </div>
+    </main>
   )
 }
 
