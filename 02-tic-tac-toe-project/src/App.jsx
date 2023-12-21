27333c88b9d7c7f90f12f717c9238c2efb85f18f
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2023-12-22 03:15:48 +0530

    added edit button function

--- 02-tic-tac-toe-project/src/App.jsx
+++ 02-tic-tac-toe-project/src/App.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import Player from "./components/player";
-function App() {
-  
 
+function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
-          
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
         GAME BOARD
       </div>
       LOG
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- 02-tic-tac-toe-project/src/components/Player.jsx
+++ 02-tic-tac-toe-project/src/components/Player.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
-function Player({name,symbol}) {
+function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value); // Corrected from Value to value
+  }
+
+  let editablePlayerName = (
+    <span className="player-name">{playerName}</span>
+  );
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName} 
+        onChange={handleChange}
+      />
+    );
+  }
+
   return (
     <li>
-          <span className="player">
-          <span className="player-name">{name}</span>
-          <span className="player-symbol">{symbol}</span>
-
-          </span>
-          <button>Edit</button>
-         </li>
-
-  )
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>
+        {isEditing ? 'Save' : 'Edit'}
+      </button>
+    </li>
+  );
 }
 
-export default Player
+export default Player;
