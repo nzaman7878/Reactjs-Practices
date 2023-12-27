f1181797b3d0d2f6a800cd78698b23578a8c3589
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2023-12-28 04:56:55 +0530

    outputing results in a list

--- 03-Investment-Calculator/src/App.jsx
+++ 03-Investment-Calculator/src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from "react";
-import React from "react"
-import Header from "./components/Header"
-import UserInput from "./components/UserInput"
-import Result from "./components/Result";
+import { useState } from 'react';
+
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
+
 function App() {
   const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
-});
+  });
 
-function handleChange(inputIdentifier, newValue) {
-    setUserInput(prevUserInput => {
-        return {
-            ...prevUserInput,
-            [inputIdentifier]: newValue
-        };
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
     });
-}
+  }
 
-
-  return ( 
+  return (
     <>
       <Header />
-      <UserInput userInput={userInput} onChange={handleChange}/>
-      <Result />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
