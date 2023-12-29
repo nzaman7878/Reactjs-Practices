fb7b1c8bb890f8ccc35eb7b2a3d671426fffa13b
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2023-12-29 14:28:27 +0530

    added header component

---
+++ 04-tailwind-css-basic/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./compoonents/Header";
+
+ 
+
+export default function App() {
+  return (
+    <>
+      <Header />
+     
+    </>
+  );
+}
---
+++ 04-tailwind-css-basic/src/compoonents/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../assets/logo.png';
+
+function Header() {
+  return (
+    <header>
+    <img src={logo} alt="A canvas" />
+    <h1>ReactArt</h1>
+    <p>A community of artists and art-lovers.</p>
+  </header>
+  )
+}
+
+export default Header
