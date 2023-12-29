ae292539ba4281b82717fb95af20e12c264c80f5
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2023-12-29 14:45:41 +0530

    added authInputs component

--- 04-tailwind-css-basic/src/App.jsx
+++ 04-tailwind-css-basic/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./compoonents/Header";
-
- 
-
+import AuthInputs from "./compoonents/AuthInputs.jsx";
 export default function App() {
   return (
     <>
       <Header />
-     
+      <main>
+        <AuthInputs />
+      </main>
     </>
   );
 }
