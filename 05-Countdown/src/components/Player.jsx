3819f6f71087f2ba5dfda6132fc08d3066bb277a
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-01-03 00:17:17 +0530

    manipulating-dom-with-refs

--- 05-Countdown/src/components/Player.jsx
+++ 05-Countdown/src/components/Player.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 // Functional component for the Player
 export default function Player() {
-  
- const [submitted, setSubmitted] = useState(false);
+  // Create a ref to hold the input element reference
+  const playerName = useRef();
 
   // State to store the entered player name
   const [enteredPlayerName, setEnteredPlayerName] = useState(null);
-
-    function handleChange(event) {
-      setSubmitted(false);
-      setEnteredPlayerName(event.target.value);
-    }
-
+  
   // Event handler for the button click
   function handleClick() {
-    setSubmitted(true);
+    // Set enteredPlayerName to the current value of the input element
+    setEnteredPlayerName(playerName.current.value);
+    // Clear the input field after setting the player name
+    playerName.current.value = '';
   }
 
   // JSX for the Player component
   return (
     <section id="player">
       {/* Display the player name or 'unknown entity' if not set */}
-      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
         {/* Input field with ref pointing to playerName variable */}
         <input
-          onChange={handleChange}
-          value={enteredPlayerName}
+          ref={playerName}
           type="text"
-
         />
         {/* Button triggers the handleClick function on click */}
         <button onClick={handleClick}>Set Name</button>
