f8a77041d150db0ff997bb16405c11aefac506c0
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-01-06 02:25:42 +0530

    finished

--- 06-Project-Management/src/components/Modal.jsx
+++ 06-Project-Management/src/components/Modal.jsx
@@ -1,31 +1,21 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
-import { createPortal } from 'react-dom';
+import { forwardRef } from 'react';
 
-import Button from './Button.jsx';
+const Input = forwardRef(function Input({ label, textarea, ...props }, ref) {
+  const classes =
+    'w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600';
 
-const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
-  const dialog = useRef();
-
-  useImperativeHandle(ref, () => {
-    return {
-      open() {
-        dialog.current.showModal();
-      },
-    };
-  });
-
-  return createPortal(
-    <dialog
-      ref={dialog}
-      className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
-    >
-      {children}
-      <form method="dialog" className="mt-4 text-right">
-        <Button>{buttonCaption}</Button>
-      </form>
-    </dialog>,
-    document.getElementById('modal-root')
+  return (
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textarea ? (
+        <textarea ref={ref} className={classes} {...props} />
+      ) : (
+        <input ref={ref} className={classes} {...props} />
+      )}
+    </p>
   );
 });
 
-export default Modal;
+export default Input;
