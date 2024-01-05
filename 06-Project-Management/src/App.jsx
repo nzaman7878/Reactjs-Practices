4f8a2f14ae0eb4772dea058d72d1786258a1547a
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-01-05 22:59:46 +0530

    added modal component

--- 06-Project-Management/src/App.jsx
+++ 06-Project-Management/src/App.jsx
@@ -4,17 +4,13 @@ import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 import ProjectsSidebar from './components/ProjectsSidebar.jsx';
 
-// Main App component
 function App() {
-  // State to manage project-related information
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
   });
 
-  // Function to handle the initiation of adding a new project
   function handleStartAddProject() {
-    // Update state to set the selectedProjectId to null
     setProjectsState((prevState) => {
       return {
         ...prevState,
@@ -23,19 +19,23 @@ function App() {
     });
   }
 
-  // Function to handle the addition of a new project
-  function handleAddProject(projectData) {
-    // Generate a random projectId
-    const projectId = Math.random();
-
-    // Create a new project with the provided data and generated id
-    const newProject = {
-      ...projectData,
-      id: projectId,
-    };
-
-    // Update state to add the new project to the projects array
+  function handleCancelAddProject() {
     setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+
       return {
         ...prevState,
         selectedProjectId: undefined,
@@ -44,27 +44,22 @@ function App() {
     });
   }
 
-  // Variable to determine the content to be rendered based on the selectedProjectId
   let content;
 
-  // If no project is selected (selectedProjectId is null), display the NewProject component
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
-  } 
-  // If no project is currently selected and not in the process of adding a new project, display the NoProjectSelected component
-  else if (projectsState.selectedProjectId === undefined) {
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
+  } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
-  // The main component returns a main element containing the ProjectsSidebar and the determined content
   return (
     <main className="h-screen my-8 flex gap-8">
-      {/* ProjectsSidebar component with the ability to initiate adding a new project and the list of projects */}
       <ProjectsSidebar
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
       />
-      {/* Rendered content based on the selectedProjectId */}
       {content}
     </main>
   );
--- 06-Project-Management/src/components/NewProject.jsx
+++ 06-Project-Management/src/components/NewProject.jsx
@@ -1,26 +1,29 @@
 import { useRef } from 'react';
 
-// Import the Input component from the specified file path
 import Input from './Input.jsx';
+import Modal from './Modal.jsx';
 
-// This is a React functional component named NewProject
-// It takes in a prop onAdd, which is a function to be called when a new project is added
-export default function NewProject({ onAdd }) {
-  // Create refs for input fields to store their values
+export default function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
 
-  // Function to handle the save action
   function handleSave() {
-    // Retrieve values from the input fields
     const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
-    // Perform validation if needed
+    if (
+      enteredTitle.trim() === '' ||
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() === ''
+    ) {
+      modal.current.open();
+      return;
+    }
 
-    // Call the onAdd function with the entered project data
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -28,36 +31,42 @@ export default function NewProject({ onAdd }) {
     });
   }
 
-  // The component returns a div element containing a menu, input fields, and save/cancel buttons
   return (
-    <div className="w-[35rem] mt-16">
-      {/* Menu for navigation or actions, positioned at the top right */}
-      <menu className="flex items-center justify-end gap-4 my-4">
-        {/* Cancel button */}
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        {/* Save button with an onClick event handler */}
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      {/* Section for input fields using the Input component */}
-      <div>
-        {/* Input for project title */}
-        <Input type="text" ref={title} label="Title" />
-        {/* Input for project description, using textarea */}
-        <Input ref={description} label="Description" textarea />
-        {/* Input for due date */}
-        <Input type="date" ref={dueDate} label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops ... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
