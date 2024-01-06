0feb72f2e2b691a9d39b63686c92cf08a23b5b6e
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-01-07 02:00:09 +0530

    added Shop component

--- 07-Cloth-Store(Context-API)/src/App.jsx
+++ 07-Cloth-Store(Context-API)/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 
 import Header from './components/Header.jsx';
-
+import Shop from './components/Shop.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
