ca3e5f49997e5913d1655b3942d5b4a8bbef299d
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-01-07 02:01:47 +0530

    finished

--- 07-Cloth-Store(Context-API)/src/components/Header.jsx
+++ 07-Cloth-Store(Context-API)/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 
-import CartModal from './Cart.jsx';
+import CartModal from './CartModal.jsx';
 
 export default function Header({ cart, onUpdateCartItemQuantity }) {
   const modal = useRef();
