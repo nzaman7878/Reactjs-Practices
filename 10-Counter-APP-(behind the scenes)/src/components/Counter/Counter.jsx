8eb787d0a8a3ddc6ac268db7ff4a3f3af5ea849e
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-01-15 00:47:58 +0530

    using keys for cmp reseting

--- 10-Counter-APP-(behind the scenes)/src/components/Counter/Counter.jsx
+++ 10-Counter-APP-(behind the scenes)/src/components/Counter/Counter.jsx
@@ -1,61 +1,31 @@
-import { useState, memo, useCallback, useMemo } from 'react';
+import { useState } from 'react';
 
-import IconButton from '../UI/IconButton.jsx';
-import MinusIcon from '../UI/Icons/MinusIcon.jsx';
-import PlusIcon from '../UI/Icons/PlusIcon.jsx';
-import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
 
-function isPrime(number) {
-  log('Calculating if is prime number', 2, 'other');
+function HistoryItem({ count }) {
+  log('<HistoryItem /> rendered', 3);
 
-  if (number <= 1) {
-    return false;
+  const [selected, setSelected] = useState(false);
+
+  function handleClick() {
+    setSelected((prevSelected) => !prevSelected);
   }
-
-  const limit = Math.sqrt(number);
-
-  for (let i = 2; i <= limit; i++) {
-    if (number % i === 0) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-const Counter = memo(function Counter({ initialCount }) {
-  log('<Counter /> rendered', 1);
-
-  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
-
-  const [counter, setCounter] = useState(initialCount);
-
-  const handleDecrement = useCallback(function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
-  }, []);
-
-  const handleIncrement = useCallback(function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
-  }, []);
 
   return (
-    <section className="counter">
-      <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{' '}
-        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
-      </p>
-      <p>
-        <IconButton icon={MinusIcon} onClick={handleDecrement}>
-          Decrement
-        </IconButton>
-        <CounterOutput value={counter} />
-        <IconButton icon={PlusIcon} onClick={handleIncrement}>
-          Increment
-        </IconButton>
-      </p>
-    </section>
+    <li onClick={handleClick} className={selected ? 'selected' : undefined}>
+      {count}
+    </li>
   );
-});
+}
 
-export default Counter;
+export default function CounterHistory({ history }) {
+  log('<CounterHistory /> rendered', 2);
+
+  return (
+    <ol>
+      {history.map((count) => (
+        <HistoryItem key={count.id} count={count.value} />
+      ))}
+    </ol>
+  );
+}
