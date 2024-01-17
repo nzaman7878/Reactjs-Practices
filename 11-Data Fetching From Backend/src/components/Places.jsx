eeb9b0f01b961ea88f903a06c9422878acb9d51b
NURUZZAMAN <nuruzzaman31032001@gmail.com>
2024-01-17 22:14:10 +0530

    extracting code

--- 11-Data Fetching From Backend/src/components/Places.jsx
+++ 11-Data Fetching From Backend/src/components/Places.jsx
@@ -1,26 +1,32 @@
-import { useState, useEffect } from 'react';
-
-import Places from './Places.jsx';
-
-export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
-  }, []);
-
+export default function Places({
+  title,
+  places,
+  fallbackText,
+  onSelectPlace,
+  isLoading,
+  loadingText,
+}) {
+  console.log(places);
   return (
-    <Places
-      title="Available Places"
-      places={availablePlaces}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
+    <section className="places-category">
+      <h2>{title}</h2>
+      {isLoading && <p className="fallback-text">{loadingText}</p>}
+      {!isLoading && places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
+      {!isLoading && places.length > 0 && (
+        <ul className="places">
+          {places.map((place) => (
+            <li key={place.id} className="place-item">
+              <button onClick={() => onSelectPlace(place)}>
+                <img
+                  src={`http://localhost:3000/${place.image.src}`}
+                  alt={place.image.alt}
+                />
+                <h3>{place.title}</h3>
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
   );
 }
